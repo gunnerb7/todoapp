1f415acdcd8f8e969b38d5a4381aba245c0b6472
Sumit Harijan <smtharijan@gmail.com>
2022-03-31 16:35:01 +0530

    final commit

--- script.js
+++ script.js
@@ -1,42 +1,27 @@
-/* Add a todo/ */
+// creating a empty array this array hold all todo list items
 
-// creating a empty array
-
-// function for rendering the todo items
-
+/*  function for rendering the todo items  */
+//  set item to localStorage to store data
 // select unorder list using class
-
+// Finally let’s add remove method if emptytodo delete property true
 // check if checked is true add done class effect otherwise as it is
-
 // create a new list
-
 // set attribute to new list
+// If the item already exists in the DOM
+// otherwise append it to the end of the list
+// disabled this after fixing a selection bug
+// list.append(newlist);
 
-/* // disabled this after fixing a selection bug
-                list.append(newlist); */
+/* Function for adding a todo */
+// creating object for storing Contact details name email etc ..
+// push a contactObject in todoItems array for store a data as a array in localstorage ( 3 )
+// add a event listner submit to the form
+// when the form is submitted addContact function is called
 
-// function for adding a todo
+/* function marking todo done */
 
-// creating a object
-
-// push new todo into a demoarray object
-
-/* disabled this because updated it show in list  */
-/* // print demoarray in console
-                console.log(demoarray); */
-
-// select form
-
-// add a event listner submit on form
-
-// select input
-
-// remove whitespace of input vlaue using trim method
-
-// statement condition for printing a input value
-
-// call a function for adding a new todo value
-
-// after submit input value will be become blank ""
+/* function for delete todo */
 
 // select entire list
+
+//  adding a add event listner when content is loaded and showing stored data array on the screen ( 3 )
